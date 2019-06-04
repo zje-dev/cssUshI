38846759c4b74fcdd60e3561b25d7e542ba01e10
zje-dev <50305531+zje-dev@users.noreply.github.com>
2019-06-04 12:11:22 -0500

    Add files via upload

--- cssUI/src/lib/formaHtml.py
+++ cssUI/src/lib/formaHtml.py
@@ -18,22 +18,25 @@ def formaHTML (canvas, isL, h, w):
 	tree = etree.parse(StringIO(f), parser).getroot()
 	body = tree[1]
 	head = tree[0]
+	jumpline = 0
 	textTag = ["b","span"]
 	for el in body:
 		if el.tag in textTag:
+			prev = body[body.index(el)]
+			print(prev)
 			try:
 				estilo = el.attrib["style"]
 				if not estilo == '':
 					css = cssForma(estilo)
-					canvas.create_text(0,0,fill=css["color"],text=el.text, anchor=NW,font=(css["font-family"],int(w / 45), 'bold'))
-				else:
-					canvas.create_text(0,0,fill="black",text=el.text, anchor=NW,font=("Helvetica",int(w / 45), 'bold'))
+					canvas.create_text(0,h / 29 * jumpline,fill=css["color"],text=el.text, anchor=NW,font=(css["font-family"],int(w / 75), 'bold'))
 			except:
 				if el.tag == "b":
-					tf = ("Helvetica",int(w / 45),"bold")
+					tf = ("Helvetica",int(w / 75),"bold")
 				else:
-					tf = ("Helvetica",int(w / 45))
-				canvas.create_text(0,0,fill="black",text=el.text, anchor=NW,font=tf)
+					tf = ("Helvetica",int(w / 75))
+				canvas.create_text(0,h / 29 * jumpline,fill="black",text=el.text, anchor=NW,font=tf)
+		if el.tag == "br":
+			jumpline += 1
 def cssForma (str):
 	cascade = str.split(";")
 	css ={
