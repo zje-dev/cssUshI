691d6b4ebbcdabfb6981cad739adebbf612306d2
zje-dev <50305531+zje-dev@users.noreply.github.com>
2019-06-04 21:12:01 -0500

    2019/6/4

--- cssUI/src/lib/formaHtml.py
+++ cssUI/src/lib/formaHtml.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from tkinter import *
 from tkinter import font as TF
 import os
@@ -20,23 +21,32 @@ def formaHTML (canvas, isL, h, w):
 	head = tree[0]
 	jumpline = 0
 	textTag = ["b","span"]
+	xp = 0
 	for el in body:
 		if el.tag in textTag:
-			prev = body[body.index(el)]
-			print(prev)
 			try:
 				estilo = el.attrib["style"]
 				if not estilo == '':
 					css = cssForma(estilo)
-					canvas.create_text(0,h / 29 * jumpline,fill=css["color"],text=el.text, anchor=NW,font=(css["font-family"],int(w / 75), 'bold'))
-			except:
+					canvas.create_text(xp,h / 29 * jumpline,fill=css["color"],text=el.text, anchor=NW,font=(css["font-family"],int(w / 75), 'bold'))
+			except Exception as err:
+				#print(err)
 				if el.tag == "b":
 					tf = ("Helvetica",int(w / 75),"bold")
 				else:
 					tf = ("Helvetica",int(w / 75))
-				canvas.create_text(0,h / 29 * jumpline,fill="black",text=el.text, anchor=NW,font=tf)
+				canvas.create_text(xp,h / 29 * jumpline,fill="black",text=el.text, anchor=NW,font=tf)
+			try:
+				prev = body[body.index(el) + 1]
+				if prev.tag == "br":
+					xp = 0
+				else:
+					xp += w / 75 * len(el.text)
+			except:
+				xp=0
 		if el.tag == "br":
 			jumpline += 1
+			xp = 0
 def cssForma (str):
 	cascade = str.split(";")
 	css ={
