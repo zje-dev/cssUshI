86f6289c59e7d89e32f0c071be13a1af49d869a2
zje-dev <50305531+zje-dev@users.noreply.github.com>
2019-06-13 17:52:32 -0500

    2019/6/12

--- cssUI/src/lib/formaHtml.py
+++ cssUI/src/lib/formaHtml.py
@@ -11,44 +11,54 @@ def sizeFormat (s, w):
 	if "px" in s:
 		z = float(s.replace("px", ""))
 	elif "%" in s:
-		z = w / (100 / float(s[0:-1]))
+		z = w / (110 / float(s[0:-1]))
 	else:
 		z = float(s)
-	return z
+	return int(z)
 class text:
+	element = None
+	bg = None
 	CSS = {}
 	fonZise = 0
 	texType = "normal"
 	text = ""
 	color = "black"
+	background_color = ""
+	padding = 0
 	def __init__(self, parent, x, y, c, w, h, t):
 		self.text = parent.text
 		def fz (me, par):
 			if par.tag == "h6":
-				me.fonZise = 6
+				me.fonZise = 8
 			elif par.tag == "h5":
-				me.fonZise = 10
+				me.fonZise = 11
 			elif par.tag == "h4":
-				me.fonZise = 14
+				me.fonZise = 15
 			elif par.tag == "h3":
-				me.fonZise = 17
+				me.fonZise = 16
 			elif par.tag == "h2":
-				me.fonZise = 20
-			elif par.tag == "h1":
 				me.fonZise = 25
+			elif par.tag == "h1":
+				me.fonZise = 29
 			else:
 				me.fonZise = 16
 		fz(self, parent)
 		if "style" in parent.attrib:
 			self.CSS = cssRead(parent.get("style"))
-			if parent.tag == "span":
-				if "font-size" in parent.get("style"):
-					self.fonZise = sizeFormat(self.CSS["font-size"], w)
+			if "padding" in parent.get("style"):
+				self.padding = sizeFormat(self.CSS["padding"], w)
+			if "font-size" in parent.get("style"):
+				self.fonZise = sizeFormat(self.CSS["font-size"], w)
+			if "background-color" in parent.get("style"):
+				self.background_color = self.CSS["background-color"]
+				if self.background_color != " ":
+					self.bg = c.create_rectangle(x, y, len(self.text) * self.fonZise, y + (self.fonZise * (1.5 + (self.padding * 0.14))), fill=self.background_color, outline="")
 		if parent.tag == "b":
 			self.texType = "bold"
 		if len(self.CSS) > 0:
 			self.color = self.CSS["color"]
-		c.create_text(x, y, text=parent.text, anchor=NW, fill=self.color, font=("Arial", self.fonZise, self.texType))
+		self.element = c.create_text(x + self.padding, y + self.padding, text=parent.text, anchor=NW, fill=self.color, font=("Arial", self.fonZise, self.texType))
+		
 class div:
 	element = None
 	CSS = {}
@@ -63,15 +73,18 @@ class div:
 				if elemit.text != None:
 					self.cx += len(elemit.text) * w / 79
 					tex = text(elemit,x,y,c,w,h,t)
-			c.itemconfigure(self.element, fill=self.CSS["background-color"])
-			self.pad = sizeFormat(self.CSS["padding"], w)
+			if self.CSS != " ":
+				c.itemconfigure(self.element, fill=self.CSS["background-color"])
+			else:
+				c.itemconfigure(self.element, fill="white")
+			self.pad = (sizeFormat(self.CSS["padding"], w) * 2)
 			c.coords(self.element, x, y, self.cx + self.pad, self.cy + self.pad)
 			global xp, yp
 			xp += self.pad
 			yp += self.pad
 def cssRead (style):
 	if style != "ł":
-		css = {"color":"black","background-color":"white","padding":"1px","font-size":"14px"}
+		css = {"color":"black","background-color":" ","padding":"1px","font-size":"14px"}
 		tcss = style.split(";")
 		for cs in tcss:
 			css[cs.split(":")[0]] = cs.split(":")[1]
@@ -100,6 +113,8 @@ def formaHTML (canvas, isL, h, w):
 				xp += ele.fonZise * len(ele.text)
 				if "style" in element.attrib:
 					tscss = cssRead(element.get("style"))
+					if "padding" in element.get("style"):
+						yp += (sizeFormat(tscss["padding"], w) * 2)
 	if "style" in tree[1].attrib:
 		canvas.configure(bg = cssRead(tree[1].get("style"))["background-color"])
 	canvas.delete("all")
