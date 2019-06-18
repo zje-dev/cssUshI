d9313e83fa2cdea9edd3df54c1538341875b3932
zje-dev <50305531+zje-dev@users.noreply.github.com>
2019-06-18 14:02:07 -0500

    2019/6/18

--- cssUI/__init__.py
+++ cssUI/__init__.py
@@ -9,8 +9,21 @@ from webbrowser import *
 from PIL import *
 from src.lib.formaHtml import formaHTML
 sys.path.append(".dev")
-from devC import dev_mode
+#from devC import dev_mode
+class editCanva:
+	def __init__ (self,c, w, h):
+		par = Tk()
+		par.title("editar")
+		par.geometry("+"+str(w)+"+10")
+		Button(par,text="←",cursor="target").grid(row=0,column=0)
+		canv = Canvas(par, width = w / 6, height = h / 6, background="white")
+		canv.grid(row=1,column=0)
+		Label(par,text="nada seleccionado").grid(row=2,column=0)
+		Button(par,text="color de texto").grid(row=3,column=0)
+		Button(par,text="color de fondo").grid(row=4,column=0)
+		par.mainloop()
 class inicio:
+	editor = None
 	root = None
 	tmp = []
 	images = []
@@ -20,6 +33,7 @@ class inicio:
 	dir = ""
 	v = None
 	openendFile = ""
+	cav = None
 	def __init__ (self, name, master, tk, base):
 		self.root = master
 		self.root.title(name)
@@ -28,7 +42,7 @@ class inicio:
 		i = 0
 		W = self.root.winfo_screenwidth()
 		H = self.root.winfo_screenheight()
-		for _ in range(5):
+		for _ in range(6):
 			self.tmp.append(Button(self.root))
 			filex = "src/img/i"+str(i)+".png"
 			self.images.append(tk.PhotoImage(Image.open(filex)))
@@ -60,6 +74,9 @@ class inicio:
 					editar(0)
 				Button(self.newRoot, command=openDir, text="selecciona una carpeta").pack()
 				Button(self.newRoot, command=crea, text="crear").pack()
+		def edi ():
+			if self.editor == None or self.cav == None:
+				self.editor = editCanva(self.cav, W, H)			
 		def organizar (arr, i):
 			for e in arr:
 				filex = "src/img/i"+str(i)+".png"
@@ -69,6 +86,8 @@ class inicio:
 					e.configure(command=newF)
 				elif i == 4:
 					e.configure(command=Open)
+				elif i == 5:
+					e.configure(command=edi)
 				i += 1
 		organizar(self.tmp, i)
 		def editar (data):
@@ -88,28 +107,34 @@ class inicio:
 					Label(isHow,text="que tipo de archivo representa?").pack()
 					def isHTML ():
 						self.root.attributes('-zoomed', True)
-						cav = Canvas(foor, background="white", width = W / 1.3, height = H / 1.3)
-						cav.grid(row=0,column=1,sticky="we")
+						self.cav = Canvas(foor, background="white", width = W / 1.3, height = H / 1.3)
+						self.cav.grid(row=0,column=1,sticky="we")
 						isHow.destroy()
 						i = 0
 						for i in range(3):
 							coma = None
 							b = Button(foor)
 							b.grid(row=1,column=i)
-						formaHTML(cav, data, int(H / 1.5), int(W / 1.5))
-						Xm =  Scrollbar(foor, orient="horizontal", command=cav.xview)
-						Xm.grid(row=1,column=1,sticky="ew")
-						Ym =  Scrollbar(foor, orient="vertical", command=cav.yview)
-						Ym.grid(row=0,column=2,sticky="ns")
-						cav.configure(yscrollcommand=Ym.set, xscrollcommand=Xm.set)
-						cav.configure(scrollregion=(0,0,0,1000))
+						formaHTML(self.cav, data, int(H / 1.5), int(W / 1.5))
+						if self.editor == None or self.cav == None:
+							self.editor = editCanva(self.cav, W, H)
+						try:
+							Xm = Scrollbar(foor, orient="horizontal", command=self.cav.xview)
+							Xm.grid(row=1,column=1,sticky="ew")
+							Ym = Scrollbar(foor, orient="vertical", command=self.cav.yview)
+							Ym.grid(row=0,column=2,sticky="ns")
+							self.cav.configure(yscrollcommand=Ym.set, xscrollcommand=Xm.set)
+							self.cav.configure(scrollregion=(0,0,0,1000))
+						except:
+							pass
 					def isCUI ():
 						isHow.destroy()
 					Button(isHow,text="HTML", command=isHTML).pack(fill=X)
 					Button(isHow,text="proyecto", command=isCUI).pack(fill=X)
 if len(sys.argv) > 1:
-	if sys.argv[1] == "-dev_console":
-		dev_mode()
+	pass
+#	if sys.argv[1] == "-dev_console":
+#		dev_mode()
 else:
 	root = Tk()
 	app = inicio("cssUshI", root, ImageTk, Tk)
